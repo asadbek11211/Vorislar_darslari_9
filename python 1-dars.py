1f98efe897ae797e5aecf0edb83fce42153cba8e
A.R <roziboyev614@gmail.com>
2025-10-09 16:27:17 +0500

    Ikkinchi dars githubga yuklandi

---
+++ python 1-dars.py
@@ -0,0 +1,31 @@
+# Bugundan Python bilan ishlaymiz
+# Print() konsolga malumot chiqarish buyrugi
+
+print('Salom \n barchangizga o\'quvchilar')
+print("mayli Salom \n dunyo o'quvchilar")
+print("""Bu nima degani o'quvchilar""")
+
+# Arfimetik amallar 
+# Qo'shish
+
+a = 12
+b = 34
+c = a + b
+print('c = ', c)
+
+# Ayirish
+v = a - b
+print('v = ', v)
+
+# Ko'paytirish
+m = a * b # kopaytirish
+l = a ** b # darajaga ko'tarish
+print('m = ', m)
+print('l = ', l)
+
+# Bo'lish
+g = 5
+p1 = g / 2 # haqiqiy qismini olish
+p2 = g // 2 # butun qismini olish
+print('p1 = ', p1)
+print('p2 = ', p2)
