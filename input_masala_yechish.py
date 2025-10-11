cd0dfff59639398b641f55c5b03994ba6f58da05
A.R <roziboyev614@gmail.com>
2025-10-11 16:40:43 +0500

    inputdan foydalanib masala yechdik

---
+++ input_masala_yechish.py
@@ -0,0 +1,15 @@
+# import math
+
+# ism = input("Ismingiz nima? :")
+# print("Assalomu alaykum Hurmatli, kelajagi buyuk", ism)
+
+# son = int(input("Ixtiyoriy son kiriting : "))
+
+# print("Kiritilgan sonning kvadrati: ", son*son)
+ 
+
+# Kvadratning tomoni berilgan uning yuzini S ni toping
+
+a = int(input("Kvadratning tomonini kiriting: "))
+S = a * a 
+print("Kvadratning yuzi: S = ", S)
