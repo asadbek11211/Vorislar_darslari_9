240230ae54f300f0fa96be7a075804a60212faf5
A.R <roziboyev614@gmail.com>
2025-10-14 17:12:24 +0500

    masala ishlash

---
+++ Masala_ishlash.py
@@ -0,0 +1,7 @@
+# A va B sonlari berilgan Ularning qiymatini almashtirib rezultat oling
+
+a = int(input("A sonini kiriting: "))
+b = int(input("B sonini kiriting: "))
+
+print(" A sonining qiymati A = ", b)
+print(" B sonining qiymati B = ", a)
