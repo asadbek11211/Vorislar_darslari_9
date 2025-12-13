b2ab10faa69aef671b57634423a336759986972c
A.R <roziboyev614@gmail.com>
2025-12-13 16:39:01 +0500

    aa

--- dictionary.py
+++ dictionary.py
@@ -1,111 +1,92 @@
 
-talaba1 = {
-    'ism': 'kamolbek',
-    'familya': 'rozmamatov',
-    'maktab': '1-maktab',
-    'qiziqish': 'futbool'
-}
-talaba2 = {
-    'ism': 'boboxon',
-    'familya': 'jumaboyev',
-    'maktab': '12-maktab',
-    'qiziqish': 'IT darslari'
-}
-talaba3 = {
-    'ism': 'elbek',
-    'familya': 'jabborov',
-    'maktab': '46-maktab',
-    'qiziqish': 'english darslari'
-}
-talaba4 = {
-    'ism': 'ixtiyor',
-    'familya': 'baxtiyorov',
-    'maktab': '26-maktab',
-    'qiziqish': 'IT darslari'
-}
-talaba5 = {
-    'ism': 'xolbek',
-    'familya': 'qurbonboyev',
-    'maktab': '1-maktab',
-    'qiziqish': 'fudboll'
-}
-talaba6 = {
-    'ism': 'ilyosbek',
-    'familya': 'qochqorboyev',
-    'maktab': '1-maktab',
-    'qiziqish': 'oshpazlik'
-}
-talaba7 = {
-    'ism': 'umidjon',
-    'familya': 'matyaqubov',
-    'maktab': '1-maktab',
-    'qiziqish': 'IT darslari'
-}
-talaba8 = {
-    'ism': 'zuhriddin',
-    'familya': 'qadamboyev',
-    'maktab': '26-maktab',
-    'qiziqish': 'IT darslari, barcha sport'
-}
-talaba9 = {
-    'ism': 'muhammadali',
-    'familya': 'ibragimov',
-    'maktab': '1-maktab',
-    'qiziqish': 'IT'
-}
-talaba10 = {
-    'ism': 'baxtiyor',
-    'familya': 'ozodov',
-    'maktab': '1-maktab',
-    'qiziqish': 'IT, elektronika'
-}
-talaba11 = {
-    'ism': 'sharofat',
-    'familya': 'jumamurodova',
-    'maktab': '1-maktab',
-    'qiziqish': 'korey tili darslari'
-}
-talaba12 = {
-    'ism': 'matluba',
-    'familya': 'olloyorova',
-    'maktab': '1-maktab',
-    'qiziqish': 'kitob oqish, rus tili darslari'
-}
-talaba13 = {
-    'ism': 'mashhura',
-    'familya': 'shomurotova',
-    'maktab': '1-maktab',
-    'qiziqish': 'IT darslari'
-}
-talaba14 = {
-    'ism': 'maftuna',
-    'familya': 'madaminova',
-    'maktab': '1-maktab',
-    'qiziqish': 'IT darslari, Ingliz tili, Fizika'
-}
-
-
-
-malumot_tiplari = {
-    'str': 'string',
-    'int': 2025,
-    'float': 2025.3,
-    'bool': True,
-    'list': ['malibu','cobalt','matiz'],
-    'tuple': ('pi','g','k','R'),
-    'dictionary':{
-        'kitoblar' : {
-            'badiy': 'sariq devni minib',
-            'hajviy': 'ikki karra ikki besh'
-        },
-        'badiy_kitob' : 'ikki eshik orasi'
+oquvchilar = {
+    'talaba1' : {
+        'ism': 'kamolbek',
+        'familya': 'rozmamatov',
+        'maktab': '1-maktab',
+        'qiziqish': 'futbool'
+    },
+    'talaba2' : {
+        'ism': 'boboxon',
+        'familya': 'jumaboyev',
+        'maktab': '12-maktab',
+        'qiziqish': 'IT darslari'
+    },
+    'talaba3' : {
+        'ism': 'elbek',
+        'familya': 'jabborov',
+        'maktab': '46-maktab',
+        'qiziqish': 'english darslari'
+    },
+    'talaba4' : {
+        'ism': 'ixtiyor',
+        'familya': 'baxtiyorov',
+        'maktab': '26-maktab',
+        'qiziqish': 'IT darslari'
+    },
+    'talaba5' : {
+        'ism': 'xolbek',
+        'familya': 'qurbonboyev',
+        'maktab': '1-maktab',
+        'qiziqish': 'fudboll'
+    },
+    'talaba6' : {
+        'ism': 'ilyosbek',
+        'familya': 'qochqorboyev',
+        'maktab': '1-maktab',
+        'qiziqish': 'oshpazlik'
+    },
+    'talaba7' : {
+        'ism': 'umidjon',
+        'familya': 'matyaqubov',
+        'maktab': '1-maktab',
+        'qiziqish': 'IT darslari'
+    },
+    'talaba8' : {
+        'ism': 'zuhriddin',
+        'familya': 'qadamboyev',
+        'maktab': '26-maktab',
+        'qiziqish': 'IT darslari, barcha sport'
+    },
+    'talaba9' : {
+        'ism': 'muhammadali',
+        'familya': 'ibragimov',
+        'maktab': '1-maktab',
+        'qiziqish': 'IT'
+    },
+    'talaba10' : {
+        'ism': 'baxtiyor',
+        'familya': 'ozodov',
+        'maktab': '1-maktab',
+        'qiziqish': 'IT, elektronika'
+    },
+    'talaba11' : {
+        'ism': 'sharofat',
+        'familya': 'jumamurodova',
+        'maktab': '1-maktab',
+        'qiziqish': 'korey tili darslari'
+    },
+    'talaba12' : {
+        'ism': 'matluba',
+        'familya': 'olloyorova',
+        'maktab': '1-maktab',
+        'qiziqish': 'kitob oqish, rus tili darslari'
+    },
+    'talaba13' : {
+        'ism': 'mashhura',
+        'familya': 'shomurotova',
+        'maktab': '1-maktab',
+        'qiziqish': 'IT darslari'
+    },
+    'talaba14' : {
+        'ism': 'maftuna',
+        'familya': 'madaminova',
+        'maktab': '1-maktab',
+        'qiziqish': 'IT darslari, Ingliz tili, Fizika'
     }
+
+
 }
-
-
-
-
-
 
 
 
