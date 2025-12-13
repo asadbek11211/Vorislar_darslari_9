9c6917f071e709a72d4e9f498b2a8905e86be244
A.R <roziboyev614@gmail.com>
2025-12-13 14:07:24 +0500

    aa

--- Masala_ishlash.py
+++ Masala_ishlash.py
@@ -1,7 +1,20 @@
 # A va B sonlari berilgan Ularning qiymatini almashtirib rezultat oling
 
-a = int(input("A sonini kiriting: "))
-b = int(input("B sonini kiriting: "))
+# a = int(input("A sonini kiriting: "))
+# b = int(input("B sonini kiriting: "))
 
-print(" A sonining qiymati A = ", b)
-print(" B sonining qiymati B =  ", a)
+# print(" A sonining qiymati A = ", b)
+# print(" B sonining qiymati B =  ", a)
+
+
+oquvchilar = []
+
+for oquvchi in range(4):
+    print(f"{oquvchi+1} - o'quvchini kiriting")
+    oquvchi = {
+        
+        'ism':input('Ism kiriting :' ),
+        'yoshi': input('yoshini kiriting : ')
+    }
+    oquvchilar.append(oquvchi)
+print(oquvchilar)
