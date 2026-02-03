8b874326f8b5f76e2c9844ed3ba4d1517c471c80
A.R <roziboyev614@gmail.com>
2026-02-03 17:57:44 +0500

    oi

---
+++ function.py
@@ -0,0 +1,19 @@
+# Pythonda function bilan tanishish
+
+# print("Assalomu alaykum")
+# a = 34
+# print(type(a))
+
+# def salom_ber(ism):
+#     print(f"Salom {ism} axvollaring yaxshimi")
+
+# salom_ber("bobur")
+# salom_ber("Kamolbek")
+
+
+def kopaytma(son1,son2):
+    """
+    Ikkita sonni ko'paytma ko'rinishida taqdim qiladigan function
+    """
+    print(f"{son1} * {son2} = {son1*son2}")
+kopaytma(9,74)
